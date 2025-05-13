e24fc4a7aff006ca2384491f2c3618fb7ec0d7c2
saylesss88 <saylesss87@proton.me>
2025-05-13 08:17:50 -0400

    extra.permalink

---
+++ templates/search_index.js
@@ -0,0 +1,11 @@
+{% set section = get_section(path="blog/_index.md") %}
+{% set index = [] %}
+{% for page in section.pages %}
+  {% set doc = {} %}
+  {% set_global doc = doc | merge(id=loop.index | string) %}
+  {% set_global doc = doc | merge(title=page.title) %}
+  {% set_global doc = doc | merge(content=page.content | striptags) %}
+  {% set_global doc = doc | merge(extra=page.extra) %}
+  {% set_global index = index | push(value=doc) %}
+{% endfor %}
+{{ elasticlunr_index(docs=index, fields=["title", "content", "extra.permalink"]) }}
